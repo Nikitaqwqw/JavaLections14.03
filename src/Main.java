1deb9b8dc2b06f2436cf946df6dacc56854f4d9a
Nikita <nksenofontov97@gmail.com>
2024-04-22 19:51:02 +0300

    added person class modified class Main, added two methods printName and printSurname

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,24 @@ public class Main {
 
     public static void main(String[] args) {
         printSomeMessage("Hello git lection");
+
+        printName("Nikita");
+        printSurname("Some surname");
+
+        Person person = new Person(1997,"Some name");
+        System.out.println(person.toString());
     }
 
     public static void printSomeMessage(String message){
         System.out.println(message);
+    }
+
+    public static void printName(String name){
+        System.out.println("Name is " + name);
+    }
+
+    public static void printSurname(String surname){
+        System.out.println("Surname is " + surname);
     }
 
 
