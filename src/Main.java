dd1672aa9453224f6c60b8f312e7373d7795d567
Nikita <nksenofontov97@gmail.com>
2024-04-22 19:26:23 +0300

    my first commit in intellij idea

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        printSomeMessage("Hello git lection");
+    }
+
+    public static void printSomeMessage(String message){
+        System.out.println(message);
+    }
+
+
+}
